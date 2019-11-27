3eeff805b20370d4130f749d1e4853825ee18b54
scooredmars <35012448+scooredmars@users.noreply.github.com>
2019-11-27 19:20:30 +0100

    Update settings.py

--- app/core/settings.py
+++ app/core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sbwf=l@!hb(%t5w_ab=upm1#^a9)z_4u3#8s138w18cdt(!rof'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
