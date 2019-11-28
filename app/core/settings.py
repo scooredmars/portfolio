80baf8c2cc7708fb9dd90cff2be6c38948613180
scooredmars <35012448+scooredmars@users.noreply.github.com>
2019-11-28 16:09:14 +0100

    Update settings.py

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,8 +1,11 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
